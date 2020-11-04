aac30891397a92a74ae5eaf727b890e5c7fa6d6d
Steve Shen <golddragon11@gmail.com>
2020-11-04 12:04:50 +0800

    Initial Commit

---
+++ iOSFourthHW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSFourthHW
+//
+//  Created by Steve Shen on 2020/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSFourthHW/iOSFourthHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFourthHWApp.swift
+//  iOSFourthHW
+//
+//  Created by Steve Shen on 2020/11/4.
+//
+
+import SwiftUI
+
+@main
+struct iOSFourthHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
